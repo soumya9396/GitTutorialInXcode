28401f2052f6aa56b39b523e807e93e93ae8c27b
Soumitra Bera <soumya9396@gmail.com>
2022-03-31 21:21:40 +0530

    comment added

--- GitTutorialInXcode/ViewController.swift
+++ GitTutorialInXcode/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .blue
+        ///Next thing to check merge the two branch
     }
 
 
